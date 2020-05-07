9f87e1f41143ff403de909b5caa02599edcc73d1
eralpkor <eralp.ny@gmail.com>
2020-05-07 13:29:34 -0400

    Fix a bug

---
+++ items.py
@@ -0,0 +1,12 @@
+# Items
+# item = {'lightsaber': Item('lightsaber', 'luminescent blade of magnetically contained plasma'),
+#         'nuts': Item('nuts', ' fruit composed of an inedible hard shell and a seed, which is generally edible'),
+#         'gold': Item('gold', 'a box of GOLD NUGGETS'),
+#         'flashlight': Item('flashlight', 'extreme high beam flashlight'),
+#         'videotape': Item('videotape', 'magnetic tape used for storing video and usually sound in addition.')}
+
+# room['outside'].items.append(item['lightsaber'])
+# room['outside'].items.append(item['flashlight'])
+# room['foyer'].items.append(item['nuts'])
+# room['overlook'].items.append(item['gold'])
+# room['narrow'].items.append(item['videotape'])
